939364052aa790e6ad91a8ab931478b896d09f63
myfreeweb <me@myfreeweb.ru>
2010-06-05 16:36:21 +0400

    Handling offline

--- traffic_factory.py
+++ traffic_factory.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding=utf-8
 import sys
+import os
 import gtk
 import pygtk
+import gnome
 import gnomeapplet
  
 pygtk.require('2.0')
@@ -23,14 +25,17 @@ class TrafficApplet(gnomeapplet.Applet):
         self.label.set_text(self.get_traffic('bnep0')) # TODO: settings
  
     def get_traffic(self, interface):
-        STAT_PATH = '/sys/class/net/' + interface + '/statistics/'
-        rxfile = open(STAT_PATH + 'rx_bytes', 'r')
-        rx_kb = self.kb(rxfile.read())
-        rxfile.close()
-        txfile = open(STAT_PATH + 'tx_bytes', 'r')
-        tx_kb = self.kb(txfile.read())
-        txfile.close()
-        return '%s kb / %s kb' % (rx_kb, tx_kb)
+        try:
+            STAT_PATH = '/sys/class/net/' + interface + '/statistics/'
+            rxfile = open(STAT_PATH + 'rx_bytes', 'r')
+            rx_kb = self.kb(rxfile.read())
+            rxfile.close()
+            txfile = open(STAT_PATH + 'tx_bytes', 'r')
+            tx_kb = self.kb(txfile.read())
+            txfile.close()
+            return '%s kb / %s kb' % (rx_kb, tx_kb)
+        except IOError:
+            return 'Offline'
 
     def callback_destroy(self, applet):
         del self.applet
@@ -39,27 +44,25 @@ def applet_factory(applet, iid):
     TrafficApplet(applet, iid)
     return True
  
-def main(args):
-    if len(sys.argv) == 2 and sys.argv[1] == 'run-in-window':
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.set_title('Traffic Monitor')
-        window.connect('destroy', gtk.main_quit)
-        applet = gnomeapplet.Applet()
-        applet_factory(applet, None)
-        applet.reparent(window)
-        window.show_all()
-        gtk.main()
-        sys.exit()
-    elif len(sys.argv) == 2 and sys.argv[1] == 'help':
-        print '''
-        --run-in-window - run applet independent of gnome-panel
-        --help - show this message'''
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'run-in-window':
+            window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+            window.set_title('Traffic Monitor')
+            window.connect('destroy', gtk.main_quit)
+            applet = gnomeapplet.Applet()
+            applet_factory(applet, None)
+            applet.reparent(window)
+            window.show_all()
+            gtk.main()
+            sys.exit()
+        elif sys.argv[1] == 'help':
+            print '''
+            --run-in-window - run applet independent of gnome-panel
+            --help - show this message'''
     else:
         gnomeapplet.bonobo_factory('OAFIID:GNOME_Traffic_Factory',
                                    TrafficApplet.__gtype__,
                                    'Traffic Applet',
                                    '0.9',
                                    applet_factory)
-
-if __name__ == '__main__':
-    main(sys.argv)
